7e205fae1452d179fb08fce8a810d46ee4a521a9
user <user@DESKTOP-F6A59OO>
2023-05-14 16:54:28 +0200

    Add project files.

---
+++ CRM/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Crm
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program do zarządzania klientami");
+            Console.WriteLine("Co chciałbyś zrobić?");
+            Console.WriteLine("1. Dodanie nowego klienta");
+            Console.WriteLine("2. Usunięcie klientaz bazy");
+            Console.WriteLine("3. Edycja istniejącego klienta");
+            // Console.WriteLine("4. Wyszukanie klienta"); Funkcja czasowo nie dostępna :)
+
+            string choice = Console.ReadLine();
+        }
+    }
+}
